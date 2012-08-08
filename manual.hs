98ae8c13ebbc79ec90141da296dc2abf5b6605d8
Abram Hindle <abram.hindle@ualberta.ca>
2012-08-07 20:09:08 -0600

    initial structure in order to get input and move (testing)

--- manual.hs
+++ manual.hs
@@ -5,14 +5,30 @@ import System.IO (hIsEOF, hGetLine, stdin)
 import System.Environment
 import GameState
 
+fullGameLoop :: Board -> IO ()
+fullGameLoop board = do
+  move <- getMoveFromStdin
+  newBoard <- (return (gameLoop move board))
+  putStrLn $ prettyBoard newBoard
+  fullGameLoop newBoard
+  
 main :: IO ()
 main = do
   args <- getArgs
   b <- readBoard (head args) 
   putStrLn $ prettyBoard b
+  fullGameLoop b
+  
+getMoveFromStdin :: IO Move
+getMoveFromStdin = do 
+  return W
 
 readBoard :: String -> IO Board
 readBoard file = board <$> readCellLists
   where readCellLists = do
           fileStr <- readFile file
           return (map stringToCells (lines fileStr))
+
+-- Do nothing --
+gameLoop :: Move -> Board -> Board
+gameLoop move board = board
